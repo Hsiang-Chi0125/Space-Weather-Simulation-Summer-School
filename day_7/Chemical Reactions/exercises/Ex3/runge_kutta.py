ba4e92165a74e862b4bdc19270801e5f767572a5
Hsiang-Chi Yeh <chi90125@gmail.com>
2022-07-27 11:17:58 -0400

    day_7
    :q
    git commit -m day_7
    git commir -m day_7 day_2/file.py day_6/Pendulum.py

--- day_7/Chemical Reactions/exercises/Ex3/runge_kutta.py
+++ day_7/Chemical Reactions/exercises/Ex3/runge_kutta.py
@@ -19,6 +19,7 @@ def explicit_RK_stepper(f,x,t,h,a,b,c):
     s = len(c)
     ks = [f(x,t)]
     x_new = x + h*b[0]*ks[0]
+    error = h*(b[0] - b_control[0])*ks[0]
     for i in range(s-1):
         y = x + h*sum(a[i][j]*ks[j] for j in range(i+1))
         ks.append(f(y, t+h*c[i+1]))
@@ -106,7 +107,23 @@ def adaptive_integrate(f, x0, tspan, h, step, rtol = 1e-8, atol = 1e-8):
             ts - time points visited during integration (list)
             xs - trajectory of the system (list of numpy arrays)
     """
-    return ... # please complete this function 
+    t, tf = tspan
+    x = x0
+    trajectory = [x]
+    ts = [t]
+    while t < tf:
+        h_eff = min(h, tf-t)
+        x_hat, error = step(f, t, x, h)
+        if error <= norm(x)*rtol + atol:
+            t += h
+            x = x_hat
+            trajectory.append(x)
+            ts,append(t)
+            h *= 2
+        else:
+            
+        
+    return  # please complete this function 
                # Hint 1: The slide contain pseudo code that should be a good 
                #         starting ground!
                # Hint 2: use the condition error > rtol*norm(x) + atol
